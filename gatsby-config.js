be5055cdbec426b36fadb2a32c6cdbff90085160
Muhammad Irsyad <irsyad@rightcliq.com.my>
2023-05-23 22:26:42 +0700

    chore: add path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ const config = require("./config.json")
 const infoData = require("./content/data/info.json")
 
 module.exports = {
+  pathPrefix: `/blog`,
   //this makes the site config available to forestry cms
   siteMetadata: {
     title: config.title,
