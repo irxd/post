8fdd5349a7d1b1ddf29391116854967e47014984
Muhammad Irsyad <irsyad@rightcliq.com.my>
2023-05-23 23:10:58 +0700

    fix: change path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const config = require("./config.json")
 const infoData = require("./content/data/info.json")
 
 module.exports = {
-  pathPrefix: `/blog`,
+  pathPrefix: `/post`,
   //this makes the site config available to forestry cms
   siteMetadata: {
     title: config.title,
